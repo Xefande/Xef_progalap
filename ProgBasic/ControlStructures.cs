23f6b3ab74bc6a95bde7f0141289494c49acbb66
Xefande <xefande@gmail.com>
2023-01-14 16:59:54 +0100

    FirstDay
    14/01/2023

---
+++ ProgBasic/ControlStructures.cs
@@ -0,0 +1,216 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World");
+
+// ELÁGAZÁSOK
+
+/*
+int ammo = 10;
+bool isInAir = false;
+
+
+if (ammo > 0 && !isInAir)
+{
+    Console.WriteLine("Bumm");
+    ammo = ammo - 1;
+
+}
+
+bool canShoo = ammo > 0 && !isInAir;
+
+if (canShoo)
+{
+
+    Console.WriteLine("BUMM");
+    ammo = ammo - 1;
+
+
+}
+else
+{
+    Console.WriteLine("KLICK");
+}
+
+
+*/
+
+//------------------------
+
+/*
+
+Console.WriteLine("Írj be egy egész számot");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+// Console.WriteLine("A szám duplája:");
+
+
+
+
+number %= 2;
+
+if (number == 0) { 
+Console.WriteLine("páros");
+}
+
+if (number == 1)
+{
+    Console.WriteLine("páratlan");
+}
+
+else {
+    Console.WriteLine("Ez nem egy egész szám, te gyökér!");
+}
+
+*/
+
+/*
+Console.WriteLine("Írj be egy egész számot");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+if (number > 0)
+{
+    Console.WriteLine("Pozitív");
+}
+
+else if (number < 0){
+    Console.WriteLine("Negatív");
+}
+
+
+else {
+    Console.WriteLine("Nulla");
+}
+
+*/
+/*
+ 
+ 
+if 
+else if
+else if
+else
+ 
+ 
+ */
+/*
+Console.WriteLine("Írj be egy egész számot");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+
+string text = number % 10 == 0 ? "Kerek" : "Nem kerek";
+
+Console.WriteLine(text);
+}
+*/
+//ciklusok
+
+
+//while
+/*
+int i = 1;
+
+while (i <= 100)
+{
+    Console.WriteLine(i);
+    i++;
+
+
+}
+*/
+
+//for ciklus
+
+/*
+int x = 3;
+
+for (int j = 1; j <= 10; j++)
+{
+
+    Console.WriteLine(x);
+    x = x + 3;
+
+
+} */
+
+//első n olyan számot, ami osztható ottal, de nem osztható 7-el
+
+
+
+/*
+
+
+int n = 20;
+int x = 5;
+int y = 0;
+
+
+
+for (int j = 1; j <= n+y; j++)
+{
+
+
+
+    if (x % 7 != 0)
+    {
+
+        Console.WriteLine(x);
+
+    }
+    else {
+        y++;
+    
+    }
+
+    x = x + 5;
+}
+
+*/
+
+
+
+// szorszótbla 
+// 1 * 1 = 1 
+// 1 * 2 = 2
+
+
+/*
+int a = 1;
+int b = 1;
+
+
+while (a <= 10) { 
+
+
+    while (b <= 10)
+    {
+        Console.WriteLine(a + "*" + b + "=" + a * b);
+        b++;
+    }
+
+    a++;
+
+    b = 1;
+}
+
+*/
+
+//Metódusok
+
+double f1 = 12.5f;
+double f2 = 3;
+
+
+double power = Math.Pow(f1,f2); //f1^f2
+double power = Math.Pow( );
+
+
+
+
---
+++ ProgBasic/Program.cs
@@ -0,0 +1,205 @@
+﻿/*
+
+// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine ("ÁRVÍZTŰRŐ tükörfúrógép");
+
+/*
+teszt szöveg
+kaki maki 
+kuki muki
+*/
+
+// test
+/*
+int age = 35; // int = integer egész szám típus
+Console.WriteLine (age);
+
+age = 32;
+Console.WriteLine (age);
+
+int height; // deklaráció
+height = 184;
+Console.WriteLine (height);
+
+
+int a = age + 5;
+Console.WriteLine (a);
+
+int b, c;
+a = 5;
+b = 7;
+c = a + b;
+c = c + 1;
+
+Console.WriteLine (c);
+
+
+
+// Műveletek
+
+
+
+int sum = a + b; //12
+int dif = a - b; //-2
+int product = a * b; //35
+int rate = a / b; //0
+
+int modulo1 = 10 % 3;  // 1 MODULO
+int modulo2 = 12 % 4;  // 0
+int modulo3 = 6 % 5;  // 1
+int modulo4 = 100 % 8;  // 4
+int modulo5 = 12 % 16;  // 12
+
+
+Console.WriteLine(modulo5);
+
+int num1 = 3 + 2 * 4; //11 
+int num2 = (3 + 2) * 4; //20
+
+int number = 0;
+number = number + 5; //5
+number += 5; //10  ugyanazt csinálja mint az előző
+
+number -= 2; // 8
+number *= 2; // 16
+
+number /= 3; //5
+number %= 3; //2 MODULO maradék 
+
+
+number += 1; //3
+number++; //4
+
+number -= 1; // 3
+number--; // 2
+
+
+
+float f1 = 12.5f;
+double d1 = 12.77;
+
+
+/*
+ int 32 bit
+ long 64 bit (hosszú int)
+   
+ float 32 bit
+ double 64
+
+ bit 1 vagy 0
+
+1 byte 8 bit
+1 byte 2^8 = 256
+  
+
+van még sok típus amit nem használunk, főleg az intet (c# dokumentácio)
+
+ */
+/*
+
+float f2 = 12.45f;
+float f3 = 3 + f2 ;
+
+
+// Casting / Kasztolás - átalakítani egyik típust másikkkba
+
+
+
+int i1 = 7;
+float f4 = 33.5f;
+
+int i2 = (int)f4; // explicit kasztolás 
+float f5 = (float) i1; // implicit - sötétkék jelzi hogy feléesleges mert magától is megcsinálja
+
+
+string mySpecialString = "Árvíztűrő tükörfúrógép";
+*/
+/*
+ változó
+- nem kezdődhet számmal
+- nem lehet benne kötőjel
+ */
+/*
+string s1 = f1 + "AAA" + " " + i1 + "BBB";
+
+string s2 = "BBB" + 3 + i1 + "AAA"; // BBB37AAA
+string s3 =  3 + i1 + "AAA" + "BBB"; // 10AAABBB
+
+
+
+int xxx = int.Parse("10");
+
+xxx += 5;
+
+int hex = 0XFF6600
+
+
+Console.WriteLine(s2);
+Console.WriteLine(xxx);
+
+
+
+bool b1 = false;
+bool b2 = true;
+
+bool b3 = i1 > f3;
+Console.WriteLine(b3);
+bool b4 = i1 < f3;
+bool b5 = i1 >= 43;
+bool b6 = 22.4f <= f3;
+bool b7 = 3 > 3; //false
+bool b8 = 3 >= 3; // true
+
+bool b9 = i1 == f3; // egyenlőe-e
+bool b10 = i1 != f3; // NEM egyenlő-e?
+
+bool b11 = "aaabbb" == "aaaBBB"; //false
+bool b12 = "aaabbb" == "aaabbb"; //true
+
+
+//-------------------------------------------------
+
+
+bool b13 = b1 == b2;
+
+bool b14 = b1 && b2; // akkor igaz, ha mindkettő igaz
+bool b15 = b1 || b2; // akkor igaz, ha legalább egyik igaz
+
+bool b16 = !b1; // NEGÁLÁS : A MÁSIK
+
+bool b17 = b1 ^ b2;  //XOR: Kizáró vagy
+
+
+// view - solution explorer - jobb gomb, add - 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
